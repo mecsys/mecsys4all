fdfee5095e028343b475db7b2aafc17a51f36e71
Isaac Mecchi <isaacmecchi@gmail.com>
2025-05-19 20:54:58 -0300

    first commit.

---
+++ src/main/java/br/com/mecsys/mecsys4all/Mecsys4allApplication.java
@@ -0,0 +1,13 @@
+package br.com.mecsys.mecsys4all;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mecsys4allApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Mecsys4allApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/mecsys/mecsys4all/Mecsys4allApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.mecsys.mecsys4all;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Mecsys4allApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
